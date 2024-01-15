b0b64c5cd7d7013b137d8e424fc6c80c6e21dd75
Phuong Quyen Le <ltpquyen02vvk@gmail.com>
2024-01-15 15:08:32 +0700

    fix loading

--- src/screens/IngredientFilter.tsx
+++ src/screens/IngredientFilter.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 import { MD3Theme, Text, TextInput, useTheme } from 'react-native-paper';
 import { IngredientEntity } from '../api/models/entities/IngredientEntity/IngredientEntity';
 import { IngredientService } from '../api/services/ingredientService';
@@ -10,7 +10,6 @@ import { useSpinner } from '../hooks';
 const IngredientFilter = ({ navigation }) => {
   const theme = useTheme();
   const styles = getStyles(theme);
-
   const spin = useSpinner();
 
   const [search, setSearch] = useState('');
@@ -19,22 +18,22 @@ const IngredientFilter = ({ navigation }) => {
   useEffect(() => {
     let active = true;
 
-    spin();
     (async () => {
       let entites;
 
+      spin();
       try {
         entites = await IngredientService.GetAll(1000000);
         console.log('entites', entites);
       } catch (error) {
         console.log('error', error);
       } finally {
-        spin(false);
       }
 
       if (!active) return;
 
       setIngredients(entites);
+      spin(false);
     })();
 
     return () => {
@@ -46,31 +45,29 @@ const IngredientFilter = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={[styles.content]}>
-        <Text variant="headlineSmall" style={{ fontWeight: 'bold' }}>
-          Tìm kiếm với Nguyên liệu
-        </Text>
-        <TastealTextInput
-          value={search}
-          onChangeText={setSearch}
-          placeholder="Tìm nguyên liệu phổ biến"
-          right={<TextInput.Icon icon="magnify" />}
-        />
-        <Text variant="titleLarge" style={{ fontWeight: 'bold' }}>
-          Nguyên liệu đã chọn
-        </Text>
+      <View style={styles.innerContainer}>
+        <View style={[styles.content]}>
+          <Text variant="headlineSmall" style={{ fontWeight: 'bold' }}>
+            Tìm kiếm với Nguyên liệu
+          </Text>
+          <TastealTextInput
+            value={search}
+            onChangeText={setSearch}
+            placeholder="Tìm nguyên liệu phổ biến"
+            right={<TextInput.Icon icon="magnify" />}
+          />
+          <Text variant="titleLarge" style={{ fontWeight: 'bold' }}>
+            Nguyên liệu đã chọn
+          </Text>
 
-        <Text variant="titleLarge" style={{ fontWeight: 'bold' }}>
-          Gợi ý cho bạn
-        </Text>
+          <Text variant="titleLarge" style={{ fontWeight: 'bold' }}>
+            Gợi ý cho bạn
+          </Text>
 
-        <FlatList
-          data={ingredients}
-          keyExtractor={(i) => i.id.toString()}
-          renderItem={({ item }) => (
-            <IngredientItem name={item.name} image={item.image} />
-          )}
-        />
+          {ingredients.map((i) => (
+            <IngredientItem key={i.id} name={i.name} image={i.image} />
+          ))}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -80,9 +77,11 @@ const getStyles = (theme?: MD3Theme) =>
   StyleSheet.create({
     container: {
       flex: 1,
+      backgroundColor: theme.colors.background,
+    },
+    innerContainer: {
       marginTop: 60,
       marginHorizontal: 20,
-      backgroundColor: theme.colors.background,
     },
     content: {
       gap: 12,
